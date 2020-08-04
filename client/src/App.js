d427bc4e992734857338fb8a1ef8c8939bcc44ab
danielazheleva <xdaniela.z@gmail.com>
2020-08-04 15:21:39 +0100

    update UI

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class App extends Component {
 
   state = {
-    result: []
+    result: ''
   }
 
   componentDidMount() {
@@ -11,13 +11,17 @@ class App extends Component {
       .then(res => res.json())
       .then((data) => {
         this.setState({result: data})
+        console.log(result)
       })
       .catch(console.log)
   }
 
   render() {
     return (
-    <h2>{this.state.result}</h2>
+    <div>
+      <h1>Message from the front end: "hello World"</h1>
+      <h2>Message from backend: {this.state.result}</h2>
+    </div>
     );
   }
 }
